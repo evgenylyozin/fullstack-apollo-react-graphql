0e47fa0ab7dd55db2424304ba861dd2823a2e72e
Evgeny Lyozin <evgenylyozindev@gmail.com>
2020-12-05 11:17:43 +0300

    initial apollo client setup

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import {
+    ApolloClient,
+    NormalizedCacheObject
+  } from '@apollo/client';
+
+import { cache } from './cache';
+  
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+    cache,
+    uri: 'http://localhost:4000/graphql'
+});
