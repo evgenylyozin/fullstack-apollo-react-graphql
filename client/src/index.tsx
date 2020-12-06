c42f0e04b6858d07c1ccf84ffe295bd313bf594e
Evgeny Lyozin <evgenylyozindev@gmail.com>
2020-12-06 08:20:14 +0300

    added login, logout, cart, booking functionality

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,23 @@
 import {
     ApolloClient,
     NormalizedCacheObject,
-    ApolloProvider
+    ApolloProvider,
+    gql,
+    useQuery
 } from '@apollo/client';
 import { cache } from './cache';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Pages from './pages';
 import injectStyles from './styles';
+import Login from './pages/login';
+
+export const typeDefs = gql`
+  extend type Query {
+    isLoggedIn: Boolean!
+    cartItems: [ID!]!
+  }
+`;
 
 // Initialize ApolloClient
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
@@ -15,15 +25,27 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
     headers: {
         authorization: localStorage.getItem('token') || '',
-    }
+    },
+    typeDefs,
 });
 
 injectStyles();
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
 <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
 </ApolloProvider>,
 document.getElementById('root')
 );
